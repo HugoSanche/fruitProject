e91e9b6eac50dcfe1bd752d1d316c32a7a4a4dcc
HugoSanche <hbmclaren@gmail.com>
2022-09-06 19:03:59 -0500

    Complete insertion and validation using mongoose

--- app.js
+++ app.js
@@ -1,67 +1,86 @@
-const MongoClient=require('mongodb').MongoClient;
-const assert=require('assert');
+const mongoose = require('mongoose');
 
-//Connection URL
-const url ='mongodb://localhost:27017';
-
-//Database Name
-const dbName='fruitsDB';
-
-//Create a new MongoClient
-const client =new MongoClient(url,{useNewUrlParser:true});
-
-//Use connect method to connect to the server
-client.connect(function(err){
-  assert.equal(null,err);
-  console.log("Connected seccessfully to server");
-  const db =client.db(dbName);
-
-  //insertDocuments(db,function(){
-findDocuments(db,function(){
-  client.close();
-  });
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true
 });
 
-const insertDocuments = function (db,callback){
-  // Get the documents collection
-  const collection =db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name:"Apple",
-      score:8,
-      review:"Great Fruit"
-    },
-    {
-      name:"Orange",
-      score:10,
-      review:"Simplemente Delicioso"
-    },
-    {
-      name:"Platano",
-      score:6,
-      review:"very sweet"
-    }
-  ],
-  function(err, result){
-    assert.equal(err,null);
-    assert.equal(3,result.insertedCount);
-    assert.equal(3,Object.keys(result.insertedIds).length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-    }
+// const personSchema=new mongoose.Schema({
+// name:String,
+// age:Number
+// });
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type:String,
+  required: true,"Why not name?"},
+  rating: {
+    type:Number, //valida que sea tipo numerico
+    min:1,      //valida que el valor minimo sea 1
+    max:10     //valida que el valor maximo sea 10
+  },
+  review: String
+})
 
-const findDocuments=function(db,callback){
+const Fruit = mongoose.model("Fruit", fruitSchema);
+//const Person=mongoose.model("Person",personSchema);
+
+const Ciruela = new Fruit({
+  //name: "Ciruela",
+  rating: 10,
+  review: "Acida pero muy rica"
+});
+
+// const Pera = new Fruit({
+//   name: "Pera",
+//   rating: 8,
+//   review: "Algo seca"
+// });
+//
+// const Orange = new Fruit({
+//   name: "Orange",
+//   rating: 10,
+//   review: "The best fruit in the word"
+// });
+
+// Fruit.insertMany([Banana,Pera,Orange], function(err){
+//     if (err){
+//       console.log("Se presento un error");
+//     }
+//     else{
+//       console.log("Registro Actulizado");
+//     }
+// });
+
+Ciruela.save();
+
+// Fruit.find(function(err, fruits) {
+//   if (err) {
+//     console.log("Ups something happens!");
+//   } else {
+//     //Al ejecutarse callback el arreglo fruits ya tiene todos los elementos, no se debe cerrar despues Del
+//     //foreach porque te imaginas si tendria que iterar 1 millon de veces? se cerraria despues de la iteracion
+//     mongoose.connection.close();
+//     fruits.forEach((item, i, x) => {
+//       console.log(item.name);
+//     });
+//   }
+// });
+
+
+// const person = new Person({
+//   name:"John",
+//   age:37
+// });
+
+// person.save();
+
+const findDocuments = function(db, callback) {
   //Get the documents collection
-  const collection=db.collection('fruits');
+  const collection = db.collection('fruits');
   // Find some Documents
-  collection.find({}).toArray(function(err,fruits)
-  {
-    assert.equal(err,null);
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
     console.log("Found the following records");
     console.log(fruits);
     callback(fruits);
-  }
-)
+  })
 }
